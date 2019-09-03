db43ba3e737ce1c8475bf009e5c1b0de107761a0
User1 <user1@Skycliffitdomain.com>
2019-09-03 19:19:41 +0530

    updated

--- src/app/sync/sync.component.ts
+++ src/app/sync/sync.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+// tslint:disable-next-line: max-line-length
 import { View, EventSettingsModel, GroupModel, ResourceDetails, TreeViewArgs, ActionEventArgs, EventFieldsMapping, PopupOpenEventArgs, RenderCellEventArgs } from '@syncfusion/ej2-schedule';
-import { extend } from 'webdriver-js-extender';
+
 import { doctorData } from '../data';
 import { ScheduleComponent } from '@syncfusion/ej2-angular-schedule';
 import { addClass } from '@syncfusion/ej2-base';
+import { extend } from 'webdriver-js-extender';
+
 
 @Component({
   selector: 'app-sync',
@@ -13,18 +16,18 @@ import { addClass } from '@syncfusion/ej2-base';
 export class SyncComponent {
   public data: Object[] = <Object[]>extend([], doctorData);
     public selectedDate: Date = new Date(2018, 3, 1);
+
     public currentView: View = 'WorkWeek';
     public allowResizing: boolean = false;
     public allowDragDrop: boolean = false;
     public resourceDataSource: Object[] = [
 
-        { text: 'Hema@ 38 Astoria', id: 1, color: '',  workDays: [1, 2],startHour: '02:00', endHour: '07:00' },
-        { text: 'Hema@ 45 DEAN', id: 2, color: '', workDays: [1, 3, 5], startHour: '12:00', endHour: '08:00' },
-        { text: 'Hema@ 45 West NY ', id: 3, color: '', startHour: '11:00', endHour: ':00' }
+        { text: 'Hema@ 38 Astoria', id: 1, color: '#ea7a57',  workDays: [1, 2],startHour: '02:00', endHour: '07:00', },
+        { text: 'Hema@ 45 DEAN', id: 2, color: 'rgb(53, 124, 210)', workDays: [1, 3, 5], startHour: '12:00', endHour: '08:00'},
+        { text: 'Hema@ 45 West NY ', id: 3, color: 'pink', startHour: '11:00', endHour: ':00' },
     ];
     public group: GroupModel = { resources: ['Doctors'] };
-    public eventSettings: EventSettingsModel = {
-        dataSource: this.data,
+    public eventSettings: EventSettingsModel = {dataSource: this.data,
         fields: {
             subject: { title: 'Service Type', name: 'Subject' },
             location: { title: 'Patient Name', name: 'Location' },
@@ -43,10 +46,10 @@ export class SyncComponent {
         (value as ResourceDetails).resourceData[(value as ResourceDetails).resource.textField] as string
         : (value as TreeViewArgs).resourceName;
 }
-getDoctorImage(value: ResourceDetails | TreeViewArgs): string {
-    let resourceName: string = this.getDoctorName(value);
-    return resourceName.replace(' ', '-').toLowerCase();
-}
+// getDoctorImage(value: ResourceDetails | TreeViewArgs): string {
+//     let resourceName: string = this.getDoctorName(value);
+//     return resourceName.replace(' ', '-').toLowerCase();
+// }
 getDoctorLevel(value: ResourceDetails | TreeViewArgs): string {
     let resourceName: string = this.getDoctorName(value);
     return (resourceName === 'Will Smith') ? 'Capiola david' :  ( resourceName === '') ? '' : 'Dr Capiola David';
@@ -55,7 +58,7 @@ getDoctorLevel(value: ResourceDetails | TreeViewArgs): string {
 onActionBegin(args: ActionEventArgs): void {
     let isEventChange: boolean = (args.requestType === 'eventChange');
     if ((args.requestType === 'eventCreate' && (<Object[]>args.data).length > 0) || isEventChange) {
-        let eventData: { [key: string]: Object } = (isEventChange) ? args.data as { [key: string]: Object } :
+        let eventData: { [key: string]: Object } = (isEventChange) ? args.data as { [key: string]: Object }:
             args.data[0] as { [key: string]: Object };
         let eventField: EventFieldsMapping = this.scheduleObj.eventFields;
         let startDate: Date = eventData[eventField.startTime] as Date;
@@ -70,8 +73,8 @@ onActionBegin(args: ActionEventArgs): void {
 
 isValidateTime(startDate: Date, endDate: Date, resIndex: number): boolean {
     let resource: ResourceDetails = this.scheduleObj.getResourcesByIndex(resIndex);
-    let startHour: number = parseInt(resource.resourceData.startHour.toString().slice(0, 2), 10);
-    let endHour: number = parseInt(resource.resourceData.endHour.toString().slice(0, 2), 10);
+    let startHour: number = parseInt(resource.resourceData.startHour.toString().slice(0, 0), 1);
+    let endHour: number = parseInt(resource.resourceData.endHour.toString().slice(0, 0), 1);
     return (startHour <= startDate.getHours() && endHour >= endDate.getHours());
 }
 
