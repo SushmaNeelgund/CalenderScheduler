3e2f6459ebfb95059b80fe1d90a5fdba87cc6aef
User1 <user1@Skycliffitdomain.com>
2019-09-04 08:58:24 +0530

    updated

--- src/app/sync/sync.component.ts
+++ src/app/sync/sync.component.ts
@@ -14,6 +14,8 @@ import { extend } from 'webdriver-js-extender';
   styleUrls: ['./sync.component.css']
 })
 export class SyncComponent {
+// for container
+
   public data: Object[] = <Object[]>extend([], doctorData);
     public selectedDate: Date = new Date(2018, 3, 1);
 
@@ -24,9 +26,11 @@ export class SyncComponent {
 
         { text: 'Hema@ 38 Astoria', id: 1, color: '#ea7a57',  workDays: [1, 2],startHour: '02:00', endHour: '07:00', },
         { text: 'Hema@ 45 DEAN', id: 2, color: 'rgb(53, 124, 210)', workDays: [1, 3, 5], startHour: '12:00', endHour: '08:00'},
-        { text: 'Hema@ 45 West NY ', id: 3, color: 'pink', startHour: '11:00', endHour: ':00' },
+        { text: 'Hema@ 45 West NY ', id: 3, color: 'green', startHour: '11:00', endHour: ':00' },
     ];
-    public group: GroupModel = { resources: ['Doctors'] };
+
+    // For displaying Providers and Locations
+     public group: GroupModel = { resources: ['Doctors'] };
     public eventSettings: EventSettingsModel = {dataSource: this.data,
         fields: {
             subject: { title: 'Service Type', name: 'Subject' },
@@ -37,7 +41,8 @@ export class SyncComponent {
         }
     };
     // @ViewChild('scheduleObj')
-    public scheduleObj: ScheduleComponent;
+    //Scheduled Appointemnets
+        public scheduleObj: ScheduleComponent;
 
   constructor() { }
 
@@ -50,26 +55,27 @@ export class SyncComponent {
 //     let resourceName: string = this.getDoctorName(value);
 //     return resourceName.replace(' ', '-').toLowerCase();
 // }
+//For fetching providers and location
 getDoctorLevel(value: ResourceDetails | TreeViewArgs): string {
     let resourceName: string = this.getDoctorName(value);
     return (resourceName === 'Will Smith') ? 'Capiola david' :  ( resourceName === '') ? '' : 'Dr Capiola David';
 }
-
-onActionBegin(args: ActionEventArgs): void {
-    let isEventChange: boolean = (args.requestType === 'eventChange');
-    if ((args.requestType === 'eventCreate' && (<Object[]>args.data).length > 0) || isEventChange) {
-        let eventData: { [key: string]: Object } = (isEventChange) ? args.data as { [key: string]: Object }:
-            args.data[0] as { [key: string]: Object };
-        let eventField: EventFieldsMapping = this.scheduleObj.eventFields;
-        let startDate: Date = eventData[eventField.startTime] as Date;
-        let endDate: Date = eventData[eventField.endTime] as Date;
-        let resourceIndex: number = [1, 2, 3].indexOf(eventData.DoctorId as number);
-        args.cancel = !this.isValidateTime(startDate, endDate, resourceIndex);
-        if (!args.cancel) {
-            args.cancel = !this.scheduleObj.isSlotAvailable(startDate, endDate, resourceIndex);
-        }
-    }
-}
+//Not getting for what it is working
+// onActionBegin(args: ActionEventArgs): void {
+//     let isEventChange: boolean = (args.requestType === 'eventChange');
+//     if ((args.requestType === 'eventCreate' && (<Object[]>args.data).length > 0) || isEventChange) {
+//         let eventData: { [key: string]: Object } = (isEventChange) ? args.data as { [key: string]: Object }:
+//             args.data[0] as { [key: string]: Object };
+//         let eventField: EventFieldsMapping = this.scheduleObj.eventFields;
+//         let startDate: Date = eventData[eventField.startTime] as Date;
+//         let endDate: Date = eventData[eventField.endTime] as Date;
+//         let resourceIndex: number = [1, 2, 3].indexOf(eventData.DoctorId as number);
+//         args.cancel = !this.isValidateTime(startDate, endDate, resourceIndex);
+//         if (!args.cancel) {
+//             args.cancel = !this.scheduleObj.isSlotAvailable(startDate, endDate, resourceIndex);
+//         }
+//     }
+// }
 
 isValidateTime(startDate: Date, endDate: Date, resIndex: number): boolean {
     let resource: ResourceDetails = this.scheduleObj.getResourcesByIndex(resIndex);
@@ -78,11 +84,11 @@ isValidateTime(startDate: Date, endDate: Date, resIndex: number): boolean {
     return (startHour <= startDate.getHours() && endHour >= endDate.getHours());
 }
 
-onPopupOpen(args: PopupOpenEventArgs): void {
-    if (args.target && args.target.classList.contains('e-work-cells')) {
-        args.cancel = !args.target.classList.contains('e-work-hours');
-    }
-}
+// onPopupOpen(args: PopupOpenEventArgs): void {
+//     if (args.target && args.target.classList.contains('e-work-cells')) {
+//         args.cancel = !args.target.classList.contains('e-work-hours');
+//     }
+// }
 
 onRenderCell(args: RenderCellEventArgs): void {
     if (args.element.classList.contains('e-work-hours') || args.element.classList.contains('e-work-cells')) {
